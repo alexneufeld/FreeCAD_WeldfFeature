ab26f91d523f6924a3a8f2e94043b962e892fa3f
Alex Neufeld <alex.d.neufeld@gmail.com>
2024-01-29 21:56:20 -0700

    correct setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,22 @@
 from setuptools import setup
 import os
-# from freecad.workbench_starterkit.version import __version__
-# name: this is the name of the distribution.
-# Packages using the same name here cannot be installed together
+from freecad.weldfeature.version import __version__
 
 version_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), 
-                            "freecad", "workbench_starterkit", "version.py")
+                            "freecad", "weldfeature", "version.py")
 with open(version_path) as fp:
     exec(fp.read())
 
-setup(name='freecad.workbench_starterkit',
-      version=str(__version__),
-      packages=['freecad',
-                'freecad.workbench_starterkit'],
-      maintainer="looooo",
-      maintainer_email="sppedflyer@gmail.com",
-      url="https://github.com/FreeCAD/Workbench-Starterkit",
-      description="template for a freecad extensions, installable with pip",
-      install_requires=['numpy'], # should be satisfied by FreeCAD's system dependencies already
-      include_package_data=True)
+setup(
+    name='freecad.weldfeature',
+    version=str(__version__),
+    packages=['freecad',
+            'freecad.weldfeature'],
+    maintainer="Alex Neufeld",
+    maintainer_email="alex.d.neufeld@gmail.com",
+    url="https://github.com/alexneufeld/FreeCAD_WeldfFeature",
+    description="FreeCAD module for adding visual reprentations "
+    "of welded joints to assemblies",
+    install_requires=[],
+    include_package_data=True
+)
